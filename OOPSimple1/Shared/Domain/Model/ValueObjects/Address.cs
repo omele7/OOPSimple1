a17f071c77e2b7abe814f741db1047058456115a
diego.cabrera <dicabrerabuitron@gmail.com>
2024-08-22 16:20:22 -0500

    feat(address): added address value object.

---
+++ OOPSimple1/Shared/Domain/Model/ValueObjects/Address.cs
@@ -0,0 +1,6 @@
+namespace OOPSimple1.Shared.Domain.Model.ValueObjects;
+
+public record Address(string Street, string Number, string City, string State, string ZipCode, string Country)
+{
+    public string AddressAsString => $"{Street} {Number}, {City}, {State}, {ZipCode}, {Country}";
+}
